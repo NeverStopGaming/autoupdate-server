5688b43f9373982137f0c41db837d9a1c2a9cca0
ChaotenHG <chaoten@neverstopgaming.net>
2021-12-31 14:48:21 +0100

    feat: init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/update', (req, res) => {
+
+    let pluginName = req.query.plugin;
+
+    //read the json file plugins.json
+    let plugins = require('./plugins.json');
+
+    res.json({"version": plugins[pluginName]});
+
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
