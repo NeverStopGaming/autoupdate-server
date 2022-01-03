fb01c07618dfa610c78df87e79fb1610bf42be14
SteinGaming <69219673+SteinGaming@users.noreply.github.com>
2022-01-03 21:06:12 +0100

    Made reading dynamic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
+const fs = require('fs')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -9,12 +10,10 @@ app.get('/update', (req, res) => {
     let pluginName = req.query.plugin;
 
     //read the json file plugins.json
-    let plugins = require('./plugins.json');
-
-    res.json(plugins[pluginName]);
+    let plugins = fs.readFileSync('src/plugins.json', 'utf-8');
+    res.json(JSON.parse(plugins)[pluginName]);
 
 });
-
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
